594def4ed205207e6f02f86ec434d235cc25033d
Martin Skinner <martin@artcom.de>
2014-07-11 22:36:06 +0200

    added support for creating yml files, now does cap deploy

--- lib/vagrant/capistrano/config.rb
+++ lib/vagrant/capistrano/config.rb
@@ -6,15 +6,24 @@ module VagrantPlugins
     class Config < Vagrant.plugin(2, :config)
       attr_accessor :capfile
       attr_accessor :rvm_string
+      attr_accessor :stage
+      attr_accessor :application_dir
+      attr_accessor :config_files
 
       def initialize
         @capfile = UNSET_VALUE
         @rvm_string = UNSET_VALUE
+        @stage = UNSET_VALUE
+        @application_dir = UNSET_VALUE
+        @config_files = {}
       end
 
       def finalize!
         @capfile   = nil if @capfile == UNSET_VALUE
         @rvm_string   = nil if @rvm_string == UNSET_VALUE
+        @stage = nil if @stage == UNSET_VALUE
+        @application_dir = nil if @application_dir == UNSET_VALUE
+        @config_files = nil if @config_files == {}
       end
 
 
--- lib/vagrant/capistrano/provisioner.rb
+++ lib/vagrant/capistrano/provisioner.rb
@@ -4,20 +4,29 @@ module VagrantPlugins
     class Provisioner < Vagrant.plugin(2, :provisioner)
 
       def provision
-        @machine.env.ui.info "provisioning: #{@config.capfile}"
+        @machine.env.ui.info "provisioning: #{@config.capfile} with #{@config.rvm_string}"
         env = {
           "BUNDLE_GEMFILE" => nil,
           "DEPLOYMENT_USER" => @machine.ssh_info[:username],
           "SSH_IDENTITY" => @machine.ssh_info[:private_key_path].join(":"),
           "HOSTS" => "#{@machine.ssh_info[:host]}:#{@machine.ssh_info[:port]}"
         }
-        @machine.env.ui.info "calling cap with environment: #{env}"
         rvm_do = "rvm #{@config.rvm_string} do "
-        commands = []
-        commands << "cd #{File.dirname(@config.capfile)}"
+        commands = ["cd #{File.dirname(@config.capfile)}"]
         commands << "#{rvm_do} cap testing rvm:install_ruby" 
         commands << "#{rvm_do} cap testing deploy:setup" 
-        # commands << "#{rvm_do} cap testing deploy" 
+        system(env, commands.join(" && "))
+
+        # now prepare shared files
+
+        @config.config_files.each do |file, contents|
+          target_file = "#{File.join(@config.application_dir,'shared', 'config', file.to_s)}.yml"
+          @machine.env.ui.info "creating: #{target_file}"
+          @machine.communicate.execute("echo -e #{contents.to_yaml.dump} > #{target_file}")
+        end
+        # now do cap deploy
+        commands = ["cd #{File.dirname(@config.capfile)}"]
+        commands << "#{rvm_do} cap testing deploy" 
         system(env, commands.join(" && "))
       end
     end
