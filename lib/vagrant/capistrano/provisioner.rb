ba5a3f6e1422aa72bdc51fac8181a3cc2227e992
Martin Skinner <martin@artcom.de>
2014-07-18 10:40:28 +0200

    started with hiera support in capistrano

--- lib/vagrant/capistrano/provisioner.rb
+++ lib/vagrant/capistrano/provisioner.rb
@@ -4,24 +4,26 @@ module VagrantPlugins
     class Provisioner < Vagrant.plugin(2, :provisioner)
 
       def provision
-        @machine.env.ui.info "provisioning: #{@config.capfile} with #{@config.rvm_string}"
+        @machine.env.ui.info "provisioning: #{@config.capfile} with #{@config.cap_ruby_string}"
         env = {
           "BUNDLE_GEMFILE" => nil,
           "DEPLOYMENT_USER" => @machine.ssh_info[:username],
           "SSH_IDENTITY" => @machine.ssh_info[:private_key_path].join(":"),
           "HOSTS" => "#{@machine.ssh_info[:host]}:#{@machine.ssh_info[:port]}",
+          "RUBY_STRING" => @config.app_ruby_string,
+          "HIERA_CONFIG_PATH" => File.expand_path(@config.hiera_config_path),
+          "HIERA_ROOT" => File.expand_path(@config.hiera_root),
         }
-        rvm_do = "rvm #{@config.rvm_string} do "
+        rvm_do = "rvm #{@config.cap_ruby_string} do "
         commands = ["cd #{File.dirname(@config.capfile)}"]
-        commands << "#{rvm_do} cap testing rvm:install_ruby" 
-        commands << "#{rvm_do} cap testing deploy:setup"
+        commands << "#{rvm_do} cap #{@config.stage} rvm:install_ruby" 
+        commands << "#{rvm_do} cap #{@config.stage} deploy:setup"
         @config.post_setup_tasks.each do |task|
-          commands << "#{rvm_do} cap testing #{task}"
+          commands << "#{rvm_do} cap #{@config.stage} #{task}"
         end
         system(env, commands.join(" && "))
 
         # now prepare shared files
-
         @config.config_files.each do |file, contents|
           target_file = "#{File.join(@config.application_dir,'shared', 'config', file.to_s)}.yml"
           @machine.env.ui.info "creating: #{target_file}"
@@ -29,7 +31,7 @@ module VagrantPlugins
         end
         # now do cap deploy
         commands = ["cd #{File.dirname(@config.capfile)}"]
-        commands << "#{rvm_do} cap testing deploy" 
+        commands << "#{rvm_do} cap #{@config.stage} deploy" 
         system(env, commands.join(" && "))
       end
     end
